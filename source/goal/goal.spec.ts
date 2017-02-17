51dfeaa02be00a2517cf65557c72cabaea1f06fb
SamGraber <authorsamgraber@gmail.com>
2017-02-17 13:53:59 -0500

    Added view tests for the goal component
    This is mainly to see how it would work. It seems like these tests would have a tendency to be a little brittle, but it's cool that we can pretty easily test the view of a component using this technique.

--- source/goal/goal.spec.ts
+++ source/goal/goal.spec.ts
@@ -73,4 +73,63 @@ describe('Goal', () => {
 		expect(goalService.putGoal.firstCall.args[0]).to.be.null;
 		expect(goal).to.be.null;
 	});
+
+	describe('view', () => {
+		beforeEach(() => {
+			component.setState({});
+		});
+
+		it('should format the average pace as minutes and seconds', () => {
+			component.setState({ averagePace: toDate(4.5) });
+
+			const averagePaceParagraph = component.render().props.children[0];
+
+			expect(averagePaceParagraph.props.children[0]).to.equal('Your current average pace is ');
+			expect(averagePaceParagraph.props.children[1]).to.equal('4:30');
+			expect(averagePaceParagraph.props.children[2]).to.equal(' minutes per mile');
+		});
+
+		it('should provide an input for the user to set a goal if none is set', () => {
+			let input = getGoalInput(component);
+
+			expect(input.props.type).to.equal('number');
+			expect(input.props.id).to.equal('goal');
+			expect(input.props.value).to.be.empty;
+
+			component.setState({ pendingGoal: 2 });
+
+			input = getGoalInput(component);
+
+			expect(input.props.value).to.equal(2);
+
+			function getGoalInput(component: any): any {
+				const setPaceDiv = component.render().props.children[1];
+				const formGroupDiv = setPaceDiv.props.children[1];
+				return formGroupDiv.props.children[0].props.children;
+			}
+		});
+
+		it('should show the user\'s goal if one is set', () => {
+			component.setState({ goal: toDate(2.5) });
+
+			const goalParagraph = component.render().props.children[2].props.children[0];
+
+			expect(goalParagraph.props.children[0]).to.equal('Your goal is ');
+			expect(goalParagraph.props.children[1]).to.equal('2:30');
+			expect(goalParagraph.props.children[2]).to.equal(' minutes per mile');
+		});
+
+		it('should show the amount by which the user needs to improve to meet their goal', () => {
+			component.setState({ 
+				goal: toDate(2.5),
+				averagePace: toDate(6),
+			});
+
+			const improvementParagraph = component.render().props.children[2].props.children[1];
+
+			expect(improvementParagraph.props.children[0]).to.equal('To reach your goal, you will need to improve your average by ');
+			expect(improvementParagraph.props.children[1]).to.equal('3:30');
+			expect(improvementParagraph.props.children[2]).to.equal(' minutes per mile');
+		});
+	});
 });
